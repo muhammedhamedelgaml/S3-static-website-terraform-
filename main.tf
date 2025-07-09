d49cb10b1339c6d1bf454f2a3d44a56d15e5c870
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-09 10:18:53 +0300

    policy using jsondecode

--- main.tf
+++ main.tf
@@ -6,10 +6,24 @@ resource "aws_s3_bucket" "s3_website_bucket" {
 
 resource "aws_s3_bucket_policy" "s3_website_bucket_policy" {
   bucket = aws_s3_bucket.s3_website_bucket.id
-    policy = file(var.path_to_policy_json)
-    
+      policy = file(var.path_to_policy_json)
+      ##or using jsondecode instead of file
+#     policy = jsondecode(
+#     {
+#       "Version": "2012-10-17",
+#       "Statement": [
+#         {
+#           "Sid": "PublicReadGetObject",
+#           "Effect": "Allow",
+#           "Principal": "*",
+#           "Action": "s3:GetObject",
+#           "Resource": "${aws_s3_bucket.s3_website_bucket.arn}/*"
+#         }
+#       ]
+#     }
+# )
+# }
 }
-
 resource "aws_s3_bucket_public_access_block" "s3_website_bucket_public_access_block" {
   bucket = aws_s3_bucket.s3_website_bucket.id
 
